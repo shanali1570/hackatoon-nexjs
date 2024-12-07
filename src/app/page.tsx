85c1aa3f80f996d20fd260d5813f408b28c2852a
shanali1570 <mrshanali00786@gmail.com>
2024-12-07 23:05:53 +0500

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
 
+
 export default function Home() {
   return (
     <div className="bg-slate-400 text-red-500 text-4xl text-center mt-4">
-      <h1>Hackatoon Next js</h1>
+      <h1 className="">Hackatoon Next js</h1>
+
+      
     </div>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,6 +15,13 @@ const config: Config = {
       fontFamily: {
         poppins: ["var(--font-poppins)", "sans-serif"], // Use CSS variable for font
       },
+      screens: {
+        xs: "480px", // Custom `xs` breakpoint for smaller devices
+        sm: "640px", // Default small devices breakpoint
+        md: "768px", // Default medium devices breakpoint
+        lg: "1024px", // Default large devices breakpoint
+        xl: "1280px", // Default extra-large devices breakpoint
+      },
     },
   },
   plugins: [],
