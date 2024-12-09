d0401f2cbbe26af00250f7994c4974c480e0a795
shanali1570 <mrshanali00786@gmail.com>
2024-12-09 22:15:06 +0500

    updeated

--- src/app/shop/page.tsx
+++ src/app/shop/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Page = () => {
   return (
-    <div className='font-bold text-center'>No Any Task Given under progress</div>
+    <div className='font-bold text-center'>No Any Task Given under progress!</div>
   )
 }
 
